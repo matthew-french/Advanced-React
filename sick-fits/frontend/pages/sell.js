5e5d245b813d7b70f177b3083fc3e79ed5275e7c
matthew.french <matthew.french@derivco.co.uk>
2019-10-18 11:00:41 +0100

    mertial and local-dev-server to test static export

---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+const Sell = props => (
+    <div>
+        <Link naked href="index">
+            <a>
+                Home
+            </a>
+        </Link>
+    </div>
+);
+
+
+export default Sell;
---
+++ sick-fits/frontend/scripts/serve-local-static.sh
@@ -0,0 +1,3 @@
+npm run build
+npm run export
+ws --directory out --port 7777 --log.format dev --open
